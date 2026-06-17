b6586362dc75956088be02765b811de26fdf4cd0
Ulises192005 <jut.quingahuano@yavirac.edu.ec>
2026-06-17 09:00:32 -0500

    CREADO EL REGISTRO

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { LoginComponent } from './components/login/login';
 import { DashboardEmpleadoComponent } from './components/dashboard-empleado/dashboard-empleado';
 import { DashboardSecretariaComponent } from './components/dashboard-secretaria/dashboard-secretaria';
 import { DashboardGerenteComponent } from './components/dashboard-gerente/dashboard-gerente';
+// Asegúrate de importar tu componente de registro (ajusta la ruta según tus carpetas)
+import { RegistroComponent } from './components/registro/registro'; 
 
 export const routes: Routes = [
   { path: '', component: InicioComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'registro', component: RegistroComponent }, // <-- Aquí registramos la ruta oficial
   { path: 'dashboard-empleado', component: DashboardEmpleadoComponent },
   { path: 'dashboard-secretaria', component: DashboardSecretariaComponent },
   { path: 'dashboard-gerente', component: DashboardGerenteComponent }
-  // NOTA: Quitamos el '**' temporalmente para romper el bucle y ver el error real
 ];
--- src/app/components/registro/registro.ts
+++ src/app/components/registro/registro.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Router } from '@angular/router'; // <-- Importante tener 'Router' aquí
+
+// IMPORTS DE ANGULAR MATERIAL
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-registro',
   standalone: true,
-  imports: [CommonModule, RouterModule],
-  template: `<h2>Pantalla de Registro (Próximamente)</h2>`,
-  styles: []
+  imports: [
+    CommonModule, 
+    RouterModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
+  ],
+  templateUrl: './registro.html'
 })
-export class RegistroComponent {}
+export class RegistroComponent {
+
+  // Inyectamos el enrutador para poder saltar de pantalla
+  constructor(private router: Router) {}
+
+  // AQUÍ ESTÁ LA FUNCIÓN QUE TE PEDÍA EL HTML
+  alRegistrar() {
+    console.log('Formulario enviado, mandando al usuario al login...');
+    // Te redirige al login
+    this.router.navigate(['/login']); 
+  }
+}
